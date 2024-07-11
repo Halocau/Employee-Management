f4592fd799c8ea68ec7a82ba67efa5e3f17cf583
buitienquat <quatbthe176382@fpt.edu.vn>
2024-07-11 07:27:24 +0700

    basic structure

---
+++ BusinessLogicLayer/BusinessObjects/AccountMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLogicLayer.BusinessObjects;
+
+public partial class AccountMember
+{
+    public string MemberId { get; set; } = null!;
+
+    public string MemberPassword { get; set; } = null!;
+
+    public string? FullName { get; set; }
+
+    public string? EmailAddress { get; set; }
+
+    public int MemberRole { get; set; }
+
+    public virtual Role MemberRoleNavigation { get; set; } = null!;
+}
---
+++ BusinessLogicLayer/BusinessObjects/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLogicLayer.BusinessObjects;
+
+public partial class Role
+{
+    public int RoleId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<AccountMember> AccountMembers { get; set; } = new List<AccountMember>();
+}
